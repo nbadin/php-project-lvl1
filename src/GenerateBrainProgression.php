4975ce15f488d16e8319b6be390e6cbd18c6410c
nbadin <nbadin011@gmail.com>
2022-06-12 17:57:01 +0300

    step 8

---
+++ src/GenerateBrainProgression.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BrainGames\GenerateBrainProgression;
+
+function generateProgression(int $start, int $step, int $stepsCount): array
+{
+    $progression = [$start];
+    for ($i = 1; $i < $stepsCount; $i += 1) {
+        $progression[] = $start + $step * $i;
+    }
+    return $progression;
+}
+
+function generateRounds($roundsCount = 3): array
+{
+    $rules = 'What number is missing in the progression?';
+    $roundsData = [];
+    for ($i = 0; $i < $roundsCount; $i += 1) {
+        $start = rand(1, 20);
+        $step = rand(1, 20);
+        $stepsCount = rand(5, 10);
+        $progression = generateProgression($start, $step, $stepsCount);
+        $indexOfHiddenItem = array_rand($progression);
+        $answer = (string) $progression[$indexOfHiddenItem];
+        $progression[$indexOfHiddenItem] = '..';
+        $question = implode(' ', $progression);
+        $roundsData[] = [$question, $answer];
+    }
+    return [$rules, $roundsData];
+}
